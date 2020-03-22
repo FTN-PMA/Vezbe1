fe97ee3dbf70e27d8c6d7b4117589ebce17558b2
milossimic <milossimicsimo@gmail.com>
2020-03-22 19:12:16 +0100

    comments

--- Reviewer/app/src/main/java/rs/reviewer/MainActivity.java
+++ Reviewer/app/src/main/java/rs/reviewer/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package rs.reviewer;
 
 import android.content.Intent;
@@ -25,18 +9,49 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import rs.reviewer.activities.SecondActivity;
 
+/*
+* Aktivnost pravimo tako sto napravimo klasu koja nasledjuje AppCompatActivity klasu.
+* Nasledjivanjem ove klase, dobijamo metode zivotnog ciklusa Aktivnosti.
+* */
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    * Unutar onCreate metode, postavljamo izgled nase aktivnosti koristeci setContentView
+    * U ovoj metodi mozemo dobaviti sve view-e (widget-e, komponente interface-a).
+    * Moramo voditi racuna, ovde se ne sme nalaziti kod koji ce blokirati prelazak aktivnosti
+    * u naredne metode! To znaci da izvrsavanje dugackih operacija treba izbegavati ovde.
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate()",Toast.LENGTH_SHORT).show();
 
+        /*
+        * Koristeci metodu findViewById, mozemo dobaviti tacnu komponentu interface-a preko
+        * njenog jedinstvenog id-a (vise o tome kasnije). Na komponente mozemo dodavati razne
+        * akcije listenere na slican nacin kako se to radi u drugi programskim jezicima.
+        * */
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
+                * Intent je glavna klasa unutar Android-a za pokretanje ili prelazak na druge delove
+                * vase aplikacije. Da bi pokrenuli drugu aktivnost imamo dve opcije
+                * Prva opcija je eksplicitan intent, gde moramo da kazemo sa koje aktivnosti prelazimo na koju aktivnost:
+                *
+                * NPR:
+                * sa MainActivity.this prelazimo na SecondActivity.class
+                *
+                * NAPOMENA: NE KORISTITI KONSTRUKTOR PRILIKOM KREIRANJA AKTIVNOSTI!!
+                */
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                /*
+                * Pozivom startActivity metode, saljemo poruku Android-u da on za nas pokrene drugu aktivnost,
+                * nakon cega korisnik biva prebacen na novu aktivnost.
+                **/
                 startActivity(intent);
             }
         });
@@ -45,6 +60,19 @@ public class MainActivity extends AppCompatActivity {
         open_google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
+                * Drugi nacin da se pokrene neka aktivnost jeste implicitan intent.
+                * Razlika od prethodnog jeste u tome sto ovde ne kazemo na koju aktvnost zelimo da predjeno
+                * nego specificiramo parametre, pa ce nam Android ponuditi sve moguce opcije od kojih mi izaberemo
+                * onu koja nam je najbolja (u slucaju da imamo vise apliacija koje mogu da obve isti posao), ili
+                * ce pokrenuti podrazumevanu (ako je takva postavljena ili ako nema drugih aplikacija koje
+                * zadovojlvaju taj kriterijum).
+                *
+                * NPR:
+                * Ako korisnik ima instalirano vise browser-a na uredjaju, a hocemo da otvorimo http://www.google.com,
+                * Android ce nam ponuditi sve pretrazivace koji mogu da zavrse tu akciju. Ako imamo samo jedan instaliran
+                * onda ce pokrenuti taj jedan jedini.
+                * */
                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
                 startActivity(i);
 
@@ -52,31 +80,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /*
+    * onResume se poziva kada je aktivnost u fokusu i korisnik je u interakciji sa
+    * aktivnosti.
+    * */
     @Override
     protected void onResume() {
     	super.onResume();
-
         Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
     }
-    
+
+    /*
+     * onPause se poziva kada je aktivnost delimicno prekrivena.
+     * */
     @Override
     protected void onPause() {
     	super.onPause();
         Toast.makeText(this, "onPause()",Toast.LENGTH_SHORT).show();
     }
 
+    /*
+     * onPause se poziva kada je aktivnost u potpunosti prekrivena.
+     * */
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "onRestart()",Toast.LENGTH_SHORT).show();
-    }
-
+    /*
+     * onStart se poziva kada se aktivnost pokrece, nakon nje poziva se onResume
+     * i aktivnost je vidljiva..
+     * */
     @Override
     protected void onStart() {
         super.onStart();
--- Reviewer/app/src/main/java/rs/reviewer/activities/SecondActivity.java
+++ Reviewer/app/src/main/java/rs/reviewer/activities/SecondActivity.java
@@ -3,13 +3,10 @@ package rs.reviewer.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import rs.reviewer.R;
 
 public class SecondActivity extends AppCompatActivity {
@@ -31,10 +28,14 @@ public class SecondActivity extends AppCompatActivity {
         });
     }
 
+    /*
+    * Metoda onBackPressed se poziva svaki put kada korisnik
+    * klikne BACK u Toolbar-u aplikacije ili isto dugme koje se
+    * fizicki nalazi na uredjaju.
+    * */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-
         Toast.makeText(this, "onBackPressed()", Toast.LENGTH_SHORT).show();
     }
 }
